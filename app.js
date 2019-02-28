c0d51067bd4291c3b8b7710b9d0baf2ec0b578a7
Fred Silva <fredsilva@Freds-MacBook-Air.local>
2019-02-28 17:35:34 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// import express from 'express';
+// import db from './db/db';
+
+// // set up express app
+// const app = express();
+
+// // set up the port
+// const PORT = 5000;
+
+// // get all todos
+// app.get('/api/v1/todos', (req, res) => {
+//   res.status(200).send({
+//     success: 'true',
+//     message: 'todos retrieved successfully',
+//     todos: db
+//   })
+// });
+
+// // listen to
+// app.listen(PORT, () => {
+//   console.log(`server running on port ${PORT}`);
+// });
+
+console.log('Hello twice');
