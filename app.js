01bee313a0dbb940d8587fadb9238ef527847c8c
Fred Silva <fredsilva@Freds-MacBook-Air.local>
2019-03-06 12:56:13 +0000

    added POST method

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-import * as express from 'express';
+import express from 'express';
 import db from './db/db';
-import * as bodyParser from 'body-parser';
+import bodyParser from 'body-parser';
 
 /** 
  * Set up express app
@@ -34,7 +34,39 @@ app.get('/api/v1/todos', (req, res) => {
 
 /** 
  * POST
- */ 
+ * 
+ * @param req
+ * @param res
+ */
+app.post('/api/v1/todos', (req, res) => {
+
+  if(!req.body.title) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'title is required'
+    });
+  } else if(!req.body.description) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'description is required'
+    });
+  }
+
+  const todo = {
+    id: db.length + 1,
+    title: req.body.title,
+    description: req.body.description
+  }
+
+  db.push(todo);
+
+  return res.status(201).send({
+    success: 'true',
+    message: 'todo added successfully',
+    todo
+  });   
+
+}); 
 
 /** 
  * Listen to PORT
